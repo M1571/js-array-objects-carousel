8673abbb805626d6a5215e6440f92bb8d288e6b4
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-21 18:52:07 +0000

    const carousel

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // console.log('prova');
 
+/*
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -23,6 +24,13 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+*/
+
+// ----------------------------------------------
+
+const carousel = [
+    
+]
 
 // ----------------------------------------------
 
